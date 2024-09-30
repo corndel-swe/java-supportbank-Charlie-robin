7e75ad0c942bbb8ce0722d212a8772558745c223
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-09-30 10:41:26 +0100

    Completed Exercises 1

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -1,8 +1,23 @@
 package com.corndel.supportbank.exercises;
 
-// import picocli.CommandLine;
-// import picocli.CommandLine.Command;
-// import picocli.CommandLine.Parameters;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
-public class HelloWorld {
+@Command(name = "hello", subcommands = {ColorCommand.class})
+public class HelloWorld implements Runnable {
+
+    @Parameters(index = "0", defaultValue = "World")
+    private String end;
+
+    public static void main(String[] args) {
+        CommandLine commandLine = new CommandLine(new HelloWorld());
+        int exitCode = commandLine.execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("Hello, %s!%n", end);
+    }
 }
